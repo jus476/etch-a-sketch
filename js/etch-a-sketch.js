fa15a550f4596bd7c808e867906f7e5d57cff882
Justin <jus88@hotmail.com>
2022-05-06 09:55:55 -0500

    Add box items to the grid

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,16 +1,26 @@
 const NUMBER_OF_ROWS = 16;
+const NUMBER_OF_COLS = 16;
 
 function createGrid(parent) {
     for(let row = 0; row < NUMBER_OF_ROWS; row++) {
-        createRow(gridView, (row + 1));
+        const rowView = createRow(gridView);
+        for(let col = 0; col < NUMBER_OF_COLS; col++) {
+            createBox(rowView);
+        }
     }
 }
 
-function createRow(parent, row_num) {
+function createRow(parent) {
     let field = document.createElement('div');
     field.classList.add("row");
-    field.textContent = `Row ${row_num}`;
     parent.appendChild(field);
+    return field;
+}
+
+function createBox(parent) {
+    let field = document.createElement('div');
+    field.classList.add("box");
+    parent.append(field);
 }
 
 const gridView = document.querySelector('.grid');
